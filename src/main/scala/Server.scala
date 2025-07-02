40de6e45e5cb73253672cb70adff5255d6887014
Eren <77862924+Hostilian@users.noreply.github.com>
2025-07-02 15:38:19 +0300

    Update Server.scala

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -132,7 +132,7 @@ object Server {
     // Quick search with query parameters
     case GET -> Root / "api" / "bus" / "quick-search" :?
         OriginParam(origin) +& DestinationParam(destination) +& DateParam(date) =>
-      val searchDate = date.getOrElse(LocalDate.now().nn.plusDays(1))
+      val searchDate = date.getOrElse(LocalDate.now().plusDays(1)).nn
       val mockTrips = List(
         Trip(
           id = s"quick-${origin}-${destination}",
