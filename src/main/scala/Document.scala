64b6e8b0e822fa23425fecf970e0db7f3436a66d
Eren <77862924+Hostilian@users.noreply.github.com>
2025-07-02 08:45:49 +0300

    a

--- src/main/scala/Document.scala
+++ src/main/scala/Document.scala
@@ -111,6 +111,8 @@ object Document {
       case (d, Empty())                     => d
       case (Horizontal(c1), Horizontal(c2)) => Horizontal(c1 ++ c2)
       case (Vertical(c1), Vertical(c2))     => Vertical(c1 ++ c2)
+      case (Vertical(c1), d2)               => Vertical(c1 :+ d2)
+      case (d1, Vertical(c2))               => Vertical(d1 +: c2)
       case (d1, d2)                         => Vertical(List(d1, d2))
     }
   }
